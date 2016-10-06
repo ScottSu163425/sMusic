f7358edfc832863b741f6978de2bce80dacb4c5c
ScottSu <362932973@qq.com>
2016-10-06 22:52:59 +0800

    Few changes . -2016-10-06 22:52:56

---
+++ app/src/main/java/com/scott/su/smusic/widget/MarqueeTextView.java
@@ -0,0 +1,38 @@
+package com.scott.su.smusic.widget;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by asus on 2016/10/6.
+ */
+
+public class MarqueeTextView extends TextView {
+    public MarqueeTextView(Context context) {
+        super(context);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+    }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+    }
+
+}
