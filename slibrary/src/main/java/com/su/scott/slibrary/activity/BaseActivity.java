929b7e216bf1320a7b292340e931620db088b0a9
ScottSu <362932973@qq.com>
2016-12-14 22:05:46 +0800

    Improving architecture of mvp. - 2016-12-14 22:05:43

--- slibrary/src/main/java/com/su/scott/slibrary/activity/BaseActivity.java
+++ slibrary/src/main/java/com/su/scott/slibrary/activity/BaseActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Pair;
@@ -37,8 +38,8 @@ public abstract class BaseActivity<V extends IView, P extends IPresenter<V>> ext
     protected abstract P getPresenter();
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         mPresenter = getPresenter();
         mLoadingDialog = new ProgressDialog(this);
