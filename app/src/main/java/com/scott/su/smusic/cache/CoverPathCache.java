893b57b6f9f9a012cfc08b5cf5d9fba14419a84f
ScottSu <362932973@qq.com>
2017-02-12 16:33:16 +0800

    Few small adjustments  . 2017-02-12 16:30:05

--- app/src/main/java/com/scott/su/smusic/cache/CoverPathCache.java
+++ app/src/main/java/com/scott/su/smusic/cache/CoverPathCache.java
@@ -12,8 +12,8 @@ public class CoverPathCache {
     private static CoverPathCache instance;
 
     private CoverPathCache() {
-        //2M cache space;
-        coverPathCache = new LruCache<>(2 * 1024 * 1024);
+        //1M cache space;
+        coverPathCache = new LruCache<>(1 * 1024 * 1024);
     }
 
     public static CoverPathCache getInstance() {
